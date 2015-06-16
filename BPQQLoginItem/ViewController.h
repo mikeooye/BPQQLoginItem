41d90abf1c2d996f03776c83ed66fc136ebd0c0a
sMike <bp34@163.com>
2015-06-16 10:57:39 +0800

    Initial Commit

---
+++ BPQQLoginItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BPQQLoginItem
+//
+//  Created by LiHaozhen on 15/6/16.
+//  Copyright (c) 2015年 LiHaozhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
